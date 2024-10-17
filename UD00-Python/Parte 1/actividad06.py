b85accc37d522752de327c7b09ac41a7e8b74be6
Maek0s <maek0szaho@gmail.com>
2024-10-18 01:47:34 +0200

    (UD0_Parte1) Actv 6 y Actv 7 subido, Parte 1 acabada.

---
+++ UD00-Python/Parte 1/actividad06.py
@@ -0,0 +1,23 @@
+# Lista con varias listas con los valores: [altura, peso]
+lista = [
+    [180, 75],
+    [165, 70],
+    [180, 65],
+    [190, 80],
+    [165, 80],
+    [190, 70]
+]
+
+# Función clave (key function)
+# Esta función se utiliza para decidir cómo ordenar los elementos de la lista
+# Parecido al compareTo de java cuando implementamos la interfaz Comparable<T>.
+# Primero ordenamos por altura en orden descendente
+# y, en caso de igualdad en la altura, ordenamos por el peso en orden ascendente (x[1]).
+def key_function(x):
+    return (-x[0], x[1])
+
+# Usamos sorted() con la key_function para ordenar la lista.
+lista_ordenada = sorted(lista, key=key_function)
+
+# Imprimimos la lista ordenada
+print(lista_ordenada)
