96fbfc84248a7baeb43a2c1e917e344076bbdd1d
Marcos Zahonero <maek0szaho@gmail.com>
2024-11-18 18:36:59 +0100

    (UD00Parte2) Actv3 en proceso

--- UD00_Python/Parte 2/actividad03.py
+++ UD00_Python/Parte 2/actividad03.py
@@ -5,7 +5,8 @@ Descripción: Actividad 3, identificar el número de patrones.
 '''
 
 def numeroPatrones(text):
-    listasPatrones = []
+    listasPatrones = ["00","101","ABC","HO"]
+    resultadoPatrones = [0,0,0,0]
 
     ptr00 = 0 # Patrón 00
     ptr101 = 0 # Patrón 101
@@ -14,12 +15,20 @@ def numeroPatrones(text):
 
     # puedo hacer un for con todos los patrones y que checkee la iteracion para sumar
     # checkear tambien si tiene siguiente caracter y arreglar cuando son 2 caracteres en vez de 3
-    for i in range(len(text)):
-        print()
+    for i in range(len(listasPatrones)):
+        for j in range(len(text)):
+            if (text[j:len(text)] in listasPatrones[i]):
+                resultadoPatrones[i] += 1
+                print(text[j:len(text)], i, listasPatrones[i])
+                
+    return resultadoPatrones
 
-    return listasPatrones
+
 
 if __name__ == '__main__':
     # Tests #
 
-    numeroPatrones("000")
+    print(numeroPatrones("000")) # 00: 2
+    #numeroPatrones("101") # 101: 1
+    #numeroPatrones("DABCABC") # ABC: 2
+    #numeroPatrones("HOHOHO") # HO: 3
